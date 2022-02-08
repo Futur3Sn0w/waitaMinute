d5439953485779b6007a33979c6d7b0ca7908467
Adam (Futur3Sn0w) <futur3sn0w@gmail.com>
2022-02-08 08:24:58 -0500

    Publish

---
+++ js.js
@@ -0,0 +1,22 @@
+var body = document.body;
+var ooBoxe = document.getElementById('animCont');
+
+window.onload = function () {
+    ooBoxe.src = "anims/win10.html";
+    document.title = "waitaMinute | Windows 10";
+}
+
+function win8() {
+    ooBoxe.src = "anims/win8.html";
+    document.title = "waitaMinute | Windows 8(.1)";
+}
+
+function win0() {
+    ooBoxe.src = "anims/win10.html";
+    document.title = "waitaMinute | Windows 10";
+}
+
+function win1() {
+    ooBoxe.src = "anims/win11.html";
+    document.title = "waitaMinute | Windows 11";
+}
